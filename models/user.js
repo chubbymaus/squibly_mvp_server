afb7acd116561df414bd8cc4f730935fad86c316
chubbymaus <court@loftydev.com>
2018-07-20 03:38:59 -0600

    refactored error messaging using sequelize hooks

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+import bcrypt from 'bcrypt';
+
 export default (sequelize, DataTypes) => {
   const User = sequelize.define("user", {
     username: {
@@ -24,8 +26,25 @@ export default (sequelize, DataTypes) => {
         }
       }
     },
-    password: DataTypes.STRING
-  });
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [5, 100],
+          msg: 'The password needs to be between 5 and 100 characters long',
+        },
+      },
+    },
+  }, {
+    hooks: {
+      afterValidate: async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 12);
+        // eslint-disable-next-line no-param-reassign
+        user.password = hashedPassword;
+      },
+    },
+  },
+  );
 
   User.associate = (models) => {
     User.belongsToMany(models.Team, {
