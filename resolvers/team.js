0a3eea905bdbd7c696aa7d0819c540db89bcb5ae
chubbymaus <court@loftydev.com>
2018-08-09 15:38:00 -0600

    fixed transaction in team resolver

--- resolvers/team.js
+++ resolvers/team.js
@@ -14,44 +14,51 @@ export default {
       )),
   },
   Mutation: {
-    addTeamMember: requiresAuth.createResolver(async (parent, { email, teamId }, { models, user }) => {
-      try {
-        const memberPromise = models.Member.findOne(
-          { where: { teamId, userId: user.id } },
-          { raw: true },
-        );
-        const userToAddPromise = models.User.findOne({ where: { email } }, { raw: true });
-        const [member, userToAdd] = await Promise.all([memberPromise, userToAddPromise]);
-        if (!member.admin) {
+    addTeamMember: requiresAuth.createResolver(
+      async (parent, { email, teamId }, { models, user }) => {
+        try {
+          const memberPromise = models.Member.findOne(
+            { where: { teamId, userId: user.id } },
+            { raw: true },
+          );
+          const userToAddPromise = models.User.findOne({ where: { email } }, { raw: true });
+          const [member, userToAdd] = await Promise.all([memberPromise, userToAddPromise]);
+          if (!member.admin) {
+            return {
+              ok: false,
+              errors: [{ path: 'email', message: 'You cannot add members to the team' }],
+            };
+          }
+          if (!userToAdd) {
+            return {
+              ok: false,
+              errors: [{ path: 'email', message: 'Could not find user with this email' }],
+            };
+          }
+          await models.Member.create({ userId: userToAdd.id, teamId });
+          return {
+            ok: true,
+          };
+        } catch (err) {
+          console.log(err);
           return {
             ok: false,
-            errors: [{ path: 'email', message: 'You cannot add members to the team' }],
+            errors: formatErrors(err, models),
           };
         }
-        if (!userToAdd) {
-          return {
-            ok: false,
-            errors: [{ path: 'email', message: 'Could not find user with this email' }],
-          };
-        }
-        await models.Member.create({ userId: userToAdd.id, teamId });
-        return {
-          ok: true,
-        };
-      } catch (err) {
-        console.log(err);
-        return {
-          ok: false,
-          errors: formatErrors(err, models),
-        };
-      }
-    }),
+      }),
     createTeam: requiresAuth.createResolver(async (parent, args, { models, user }) => {
       try {
-        const response = await models.sequelize.transaction(async () => {
-          const team = await models.Team.create({ ...args });
-          await models.Channel.create({ name: 'general', public: true, teamId: team.id });
-          await models.Member.create({ teamId: team.id, userId: user.id, admin: true });
+        const response = await models.sequelize.transaction(async (transaction) => {
+          const team = await models.Team.create({ ...args }, { transaction });
+          await models.Channel.create(
+            { name: 'general', public: true, teamId: team.id },
+            { transaction },
+          );
+          await models.Member.create(
+            { teamId: team.id, userId: user.id, admin: true },
+            { transaction },
+          );
           return team;
         });
         return {
